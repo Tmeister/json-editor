dcfc1924631f86037595f699531275d46244c277
Enrique Chavez <noone@tmeister.net>
2015-08-31 11:28:10 -0500

    Up Version

--- src/intro.js
+++ src/intro.js
@@ -1,4 +1,4 @@
-/*! JSON Editor v0.7.22 - JSON Schema -> HTML Editor
+/*! JSON Editor v0.7.23 - JSON Schema -> HTML Editor
  * By Jeremy Dorn - https://github.com/jdorn/json-editor/
  * Released under the MIT license
  *
@@ -9,4 +9,4 @@
  * See README.md for requirements and usage info
  */
 
-(function() {
+( function() {
